59cf9a7afde5c9e7b990de4cb32ac5a1d3685b70
Francescolapiana <Lushika1219@gmail.com>
2022-10-20 19:24:30 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -5,40 +5,40 @@
 // Creare l’array di oggetti con le informazioni fornite. ♠
 
 // MILESTONE 1:
-// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto ♠
 
 // MILESTONE 2:
-// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// Stampare le stesse informazioni su DOM sottoforma di stringhe 
 
 let member1 = {
     name : "Wayne Barnett" ,
     role : "Founder & CEO" ,
-    img : '<img src="./img/wayne-barnett-founder-ceo.jpg" alt="">'
+    img : '<img class="card-img-top" src="./img/wayne-barnett-founder-ceo.jpg" alt="">'
 };
 let member2 = {
     name : "Angela Caroll" ,
     role : "Chief Editor" ,
-    img : '<img src="./img/angela-caroll-chief-editor.jpg" alt="">'
+    img : '<img class="card-img-top" src="./img/angela-caroll-chief-editor.jpg" alt="">'
 };
 let member3  = {
     name : "Walter Gordon" ,
     role : "Office Manager" ,
-    img : '<img src="./img/walter-gordon-office-manager.jpg" alt="">'
+    img : '<img class="card-img-top" src="./img/walter-gordon-office-manager.jpg" alt="">'
 };
 let member4 = {
     name : "AngelaLopez" ,
     role : "Social Media Manager" ,
-    img : '<img src="./img/angela-lopez-social-media-manager.jpg" alt="">'
+    img : '<img class="card-img-top" src="./img/angela-lopez-social-media-manager.jpg" alt="">'
 };
 let member5 = {
     name : "Scott Estrada" ,
     role : "Developer" ,
-    img : '<img src="./img/scott-estrada-developer.jpg" alt="">'
+    img : '<img class="card-img-top" src="./img/scott-estrada-developer.jpg" alt="">'
 };
 let member6 = {
     name : "Barbara Ramos" ,
     role : "Graphic Designer" ,
-    img : '<img src="./img/barbara-ramos-graphic-designer.jpg" alt="">'
+    img : '<img class="card-img-top" src="./img/barbara-ramos-graphic-designer.jpg" alt="">'
 };
 
 let staff = [
@@ -57,3 +57,63 @@ let staff = [
 // console.log(member5);
 // console.log(member6);
 console.log(staff);
+
+
+
+// CREAZIONE DELL AUTOMATISMO
+
+/* <div class="col-4">
+                <div class="card">
+                    <div ></div>
+                    <div class="card-body">
+                      <h5 class="card-title"></h5>
+                      <p class="card-text"></p>
+                    </div>
+                </div>
+</div> */
+
+const container = document.getElementById('containercards');
+
+for (let z in staff) {
+
+    // creazione col
+    let col = document.createElement('div')
+    col.classList.add('col-4')
+
+    // creazione card
+    let card = document.createElement('div')
+    card.classList.add('card')
+
+    // creazione img
+    let imgprofile = document.createElement('div')
+    imgprofile.innerHTML = staff[z].img
+
+    // creazione body
+    let body = document.createElement('div')
+    body.classList.add('card-body')
+
+    // creazione name
+    let named = document.createElement('h5')
+    named.classList.add('card-title', 'text-center')
+    named.innerHTML = staff[z].name
+
+     // creazione role
+     let roled = document.createElement('p')
+     roled.classList.add('card-text', 'text-center')
+     roled.innerHTML = staff[z].role
+
+     // creazione body2
+    let body2 = document.createElement('div')
+    body2.classList.add('card-body')
+
+    
+    container.append(col)
+    col.append(card)
+    card.append(imgprofile)
+    card.append(body)
+    card.append(named)
+    card.append(roled)
+    card.append(body2)
+}
+
+    
