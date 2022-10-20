657f1cecc92c3723edb5783c3a7a68ddb1f2ca20
Francescolapiana <Lushika1219@gmail.com>
2022-10-20 18:22:35 +0200

    Start milestone 0

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite. ♠
+
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+let member1 = {
+    name : "Wayne Barnett" ,
+    role : "Founder & CEO" ,
+    img : '<img src="./img/wayne-barnett-founder-ceo.jpg" alt="">'
+};
+let member2 = {
+    name : "Angela Caroll" ,
+    role : "Chief Editor" ,
+    img : '<img src="./img/angela-caroll-chief-editor.jpg" alt="">'
+};
+let member3  = {
+    name : "Walter Gordon" ,
+    role : "Office Manager" ,
+    img : '<img src="./img/walter-gordon-office-manager.jpg" alt="">'
+};
+let member4 = {
+    name : "AngelaLopez" ,
+    role : "Social Media Manager" ,
+    img : '<img src="./img/angela-lopez-social-media-manager.jpg" alt="">'
+};
+let member5 = {
+    name : "Scott Estrada" ,
+    role : "Developer" ,
+    img : '<img src="./img/scott-estrada-developer.jpg" alt="">'
+};
+let member6 = {
+    name : "Barbara Ramos" ,
+    role : "Graphic Designer" ,
+    img : '<img src="./img/barbara-ramos-graphic-designer.jpg" alt="">'
+};
+
+let staff = [
+    member1,
+    member2,
+    member3,
+    member4,
+    member5,
+    member6
+];
+
+// console.log(member1);
+// console.log(member2);
+// console.log(member3);
+// console.log(member4);
+// console.log(member5);
+// console.log(member6);
+console.log(staff);
